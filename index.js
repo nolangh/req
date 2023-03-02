1bdd6e601088da8d34294b5db7c7d2910e99a730
nolangh <nolanghiggins@gmail.com>
2023-03-01 20:27:14 -0700

    -a

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-//import puppeteer from "puppeteer";
-const puppeteer = require("puppeteer");
+/** This is for chrome exstentions from puppeteers website
+import puppeteer from 'puppeteer';
+import path from 'path';
+
+(async () => {
+  const pathToExtension = path.join(process.cwd(), 'my-extension');
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: [
+      `--disable-extensions-except=${pathToExtension}`,
+      `--load-extension=${pathToExtension}`,
+    ],
+  });
+  const backgroundPageTarget = await browser.waitForTarget(
+    target => target.type() === 'background_page'
+  );
+  const backgroundPage = await backgroundPageTarget.page();
+  // Test the background page as you would any other page.
+  await browser.close();
+})();
+*/
+
+import puppeteer from "puppeteer";
+//const puppeteer = require("puppeteer");
 const button = document.getElementById("testbutton");
 
 async function testFunction() {
