8fbb5e6dcfacdd62540edeebf44d1787662a910b
nolangh <nolanghiggins@gmail.com>
2023-03-01 17:11:06 -0700

    -a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const puppeteer = require("puppeteer");
+const getUrl = async () => window.location.href;
+const pageUrl = getUrl;
 
+async function scrape(pageUrl) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(pageUrl);
+}
+
+scrape();
